8d6ec2d551663d8f48fabdd461de585e7cbfaa93
jasonmatthewgarcia <jason.garcia.010@gmail.com>
2020-04-11 16:45:26 +0800

    Adding option to define port and debug mode

--- start_app.py
+++ start_app.py
@@ -1,6 +1,8 @@
 from application.app import Application
+import tornado.options
 #TODO: rename application object
 
 if __name__ == '__main__':
+    tornado.options.parse_command_line()
     app = Application()
     app.start()
